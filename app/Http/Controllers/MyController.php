a51ddab6f9c169f5fbdabd37aa74ad58a3459c03
Meen <anhminh25041997@yahoo.com>
2019-11-28 20:47:26 +0700

    Add template files

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -6,27 +6,4 @@ use Illuminate\Http\Request;
 
 use App\HTTP\Requests;
 
-class MyController extends Controller
-{
-    public function Xinchao()
-    {
-    	echo "Chao cac ban";
-    }
-
-    public function KhoaHoc($ten)
-    {
-    	echo "Khoa hoc : ".$ten;
-    	return redirect()->route('MyRoute3');
-    }
-
-    public function GetData(Request $request)
-    {
-    	//echo $request->url();
-    	if($request->is('My*'))
-    		echo "Có My";
-    	else 
-    		echo "Không có My";
-    }
-}
-
 
--- routes/web.php
+++ routes/web.php
@@ -19,47 +19,12 @@ Route::get('Khoa hoc', function(){
 	return "Xin chao cac ban";
 });
 
-Route::get('KhoaPham/Laravel',function(){
-	echo "<h1>Khoa hoc Laravel</h1>";
+Route::get('thu', function(){
+	return view('admin.theloai.danhsach');
 });
 
-Route::get('HoTen/{ten}',function($ten){
-	echo "Ten cua ban la ".$ten;
-})->where(['ten'=>'[a-zA-Z]+']);
-
-Route::get('Laravel/{ngay}',function($ngay){
-	echo "Hom nay la ngay ".$ngay;
-})->where(['ngay'=>'[0-9]+']);
-
-Route::get('Route1',['as'=>'MyRoute',function(){
-	echo "Xin chao cac ban";
-}]);
-
-Route::get('Route2',function(){
-	echo "Day la Route 2";
-})->name('MyRoute2');
-
-Route::get('GoiTen',function(){
-	return redirect()->route('MyRoute2');
+Route::get('test', function(){
+	return view ('admin.home');
 });
-
-Route::group(['prefix'=> 'MyGroup'],function(){
-	Route::get('User1',function(){
-		echo "Day la User1";
-	});
-	Route::get('User2',function(){
-		echo "Day la User2";
-	});
-	Route::get('User3',function(){
-		echo "Day la User3";
-	});
-});
-
-Route::get('GoiHam','MyController@Xinchao');
-
-Route::get('ThamSo/{ten}','MyController@KhoaHoc');
-
-Route::get('goi/Controller','MyController@GetData')->name('MyRoute3');
-
 
 
